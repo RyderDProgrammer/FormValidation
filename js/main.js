7b83ea322d7d2c526ce4793c13b4a928e1b9a057
Ryder <rdeback@gmail.com>
2021-04-15 12:07:56 -0700

    Added isTextPresent its an error catcher

--- js/main.js
+++ js/main.js
@@ -2,6 +2,17 @@ window.onload = function () {
     var regButton = document.querySelector("form > button");
     regButton.onclick = regData;
     function regData() {
-        alert("Reg button was clicked");
+        isTextPresent("firstName", "First name is required!");
+        isTextPresent("lastName", "Last name is required!");
     }
 };
+function isTextPresent(id, errMsg) {
+    var textBox = document.getElementById(id);
+    var textBoxVal = textBox.value;
+    if (textBoxVal == "") {
+        var errorSpan = textBox.nextElementSibling;
+        errorSpan.innerHTML = errMsg;
+        return false;
+    }
+    return true;
+}
--- ts/main.ts
+++ ts/main.ts
@@ -5,6 +5,25 @@ window.onload = function()
 
     function regData():void
     {
-        alert("Reg button was clicked");
+        isTextPresent("firstName","First name is required!");
+        isTextPresent("lastName","Last name is required!");
     }
+}
+
+/**
+ * Returns true if the textbox with the given id has some text inside of it.
+ * Otherwise will return false.
+ * @param id The id of the <input type ="text"> to validate
+ * @param errMsg The message to display in the sibling span of the textbox.
+ * @returns true or false value based on what is passed into the program.
+ */
+function isTextPresent(id:string, errMsg:string):boolean {
+    let textBox = <HTMLInputElement>document.getElementById(id);
+    let textBoxVal = textBox.value;
+    if (textBoxVal == "") {
+        let errorSpan = <HTMLSpanElement>textBox.nextElementSibling;
+        errorSpan.innerHTML = errMsg;
+        return false;
+    }
+    return true;
 }
